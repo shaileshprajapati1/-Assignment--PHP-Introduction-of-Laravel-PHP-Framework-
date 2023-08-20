64f18e1797791c5ffaa38ba54d901dd697c08a85
shaileshprajapati1 <shaileshprajapati966@gmail.com>
2023-08-20 19:51:58 +0530

    data

--- intro_laravel_framework.php
+++ intro_laravel_framework.php
@@ -1,1 +1,62 @@
-heloo
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction of Laravel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center mb-3 "><i>Introduction of Laravel</i></h1>
+
+        <ul>
+            <li>Laravel was developed and created by <b>Taylor Otwell </b> provide an excellent substitute for the older PHP framework named CodeIgniter.</li>
+
+        </ul>
+        <h3>What Is Laravel Used For?</h3>
+        <ul>
+            <li>Laravel is an open-source PHP framework, which is robust and easy to understand. It follows a model-view-controller design pattern.</li>
+            <li>There are a variety of tools and frameworks available to you when building a web application. However, we believe Laravel is the best choice for building modern, full-stack web applications.</li>
+            <li>Laravel has a very rich set of features which will boost the speed of web development.</li>
+            <li>If you are familiar with Core PHP and Advanced PHP, Laravel will make your task easier. It saves a lot time if you are planning to develop a website from scratch. Moreover, a website built in Laravel is secure and prevents several web attacks.</li>
+        </ul>
+        <h3>Advantages of Laravel:</h3>
+        <ul>
+            Laravel offers you the following advantages, when you are designing a web application based on it
+            <li>The web application becomes more scalable, owing to the Laravel framework.</li>
+            <li>
+                Considerable time is saved in designing the web application, since Laravel reuses the components from other framework in developing web application.
+            </li>
+            <li>
+                It includes namespaces and interfaces, thus helps to organize and manage resources.
+            </li>
+        </ul>
+        <h3>Composer:</h3><a href="https://getcomposer.org/"><b>Composer Download link</b></a>
+        <ul>
+            <li>Before creating your first Laravel project, you should ensure that your local machine has PHP and Composer installed.Third party libraries can be installed easily with help of composer.</li>
+            <li>After you have installed PHP and Composer, you may create a new Laravel project via the Composer create-project command:<b>composer create-project laravel/laravel example-app</b></li>
+        </ul>
+        <h3>Artisan:</h3>
+        <ul>
+            <li>Command line interface used in Laravel is called Artisan.</li>
+            <li>After the project has been created, start Laravel's local development server using the Laravel's Artisan CLI serve command:<b>php artisan serve</b></li>
+        </ul>
+        <h3>Features of Laravel:</h3>
+        <p>Laravel offers the following key features which makes it an ideal choice for designing web applications </p>
+        <h4><b>Modularity:</b></h4>
+        <ul>
+            <li>Laravel provides 20 built in libraries and modules which helps in enhancement of the application. Every module is integrated with Composer dependency manager which eases updates.</li>
+        </ul>
+
+
+
+
+
+
+    </div>
+</body>
+
+</html>
